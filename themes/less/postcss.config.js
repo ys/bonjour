330f6d9ad0c45cd885e875f99229333c8c10b366
Yannick Schutz <yannick.schutz@gmail.com>
2019-07-16 13:51:37 +0200

    Config font (#107)
    * Better config
    
    * Font weight
    
    * Space
    
    * Fonts and stuff
    
    * NO MORE
    
    * Add postcss
    
    * Update themes/less/layouts/partials/head.html
    
    * LOL
    
    * Whitelist zoom
    
    * Dynamic classes
    
    * Learn regex
    
    * hr is fine

---
+++ themes/less/postcss.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {
+      browsers: [
+        "last 2 versions",
+        "Explorer >= 8",
+      ]
+    },
+    '@fullhuman/postcss-purgecss': {
+      content: [
+        '**/*.html',
+      ],
+      whitelist: ["hr", "medium-zoom--open", "fullwidth", "vertical"],
+      whitelistPatterns: [/always-/, /max-/]
+    },
+  },
+}
