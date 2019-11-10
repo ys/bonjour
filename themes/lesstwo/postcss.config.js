aad3723d6e00073f9f7e345e5dc4d32f100c1490
Yannick Schutz <hello@yannickschutz.com>
2019-11-10 20:35:11 +0100

    New theme new day

---
+++ themes/lesstwo/postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {
+      browsers: [
+        "last 2 versions",
+        "Explorer >= 8",
+      ]
+    },
+  },
+}
---
+++ themes/lesstwo/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    screens: {
+      'sm': '577px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1200px',
+    },
+    extend: {}
+  },
+  variants: {}
+}
