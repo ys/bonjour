2c8abcc873e785a1a26d1f2f6ccf983f77b21deb
Yannick Schutz <yschutz@salesforce.com>
2019-11-22 18:11:01 +0100

    Too many changes

--- themes/lesstwo/tailwind.config.js
+++ themes/lesstwo/tailwind.config.js
@@ -10,20 +10,17 @@ module.exports = {
     },
     extend: {
       fontFamily: {
-        serif: ["Minion", ...defaultTheme.fontFamily.serif],
-        sans: ['"Source Sans Pro"', ...defaultTheme.fontFamily.sans]
+        serif: ["freight-text-pro", ...defaultTheme.fontFamily.serif]
       },
       fontSize: {
-        xs: "0.875rem",
-        sm: "1rem",
-        base: "1.125rem",
-        lg: "1.25rem",
-        xl: "1.5rem",
-        "2xl": "1.875rem",
-        "3xl": "2.25rem",
-        "4xl": "3rem",
-        "5xl": "4rem",
-        "6xl": "6rem"
+        xs: "1rem",
+        sm: "1.125rem",
+        base: "1.25rem",
+        lg: "1.5rem",
+        xl: "1.875rem",
+        "2xl": "2.25rem",
+        "3xl": "3rem",
+        "4xl": "4rem"
       }
     }
   },
