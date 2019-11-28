595c181e2b743eb98af37ecf66b3af84a44d831a
Yannick Schutz <yschutz@salesforce.com>
2019-11-28 12:57:32 +0100

    Infinite scroll

--- themes/simple/assets/js/main.js
+++ themes/simple/assets/js/main.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
+  var infScroll = new InfiniteScroll( '.posts', {
+    path: '.page-link[aria-label="Next"]',
+    append: 'article',
+    debug: true,
+    hideNav: '.pagination',
+
+  })
   new LazyLoad({
     elements_selector: ".lazyload"
   });
