057823e636b5f0edb73b9454fa2960f59aa89c54
Yannick Schutz <hello@yannickschutz.com>
2019-12-23 14:20:12 +0100

    Enable dark theme easily

--- themes/simple/assets/js/main.js
+++ themes/simple/assets/js/main.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
+  url = new URL(window.location.href);
+  if (url.searchParams.get('dark')) {
+    document.querySelector('html').classList.toggle('mode-dark');
+  }
   new LazyLoad({
     elements_selector: ".lazyload"
   });
