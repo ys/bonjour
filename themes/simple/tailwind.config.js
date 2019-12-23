e3d1997961225083a1ab910d6e128565d95002fb
Yannick Schutz <github@yannickschutz.com>
2019-12-23 17:00:04 +0100

    Dark (#127)
    * Test dark theme
    
    * Adapt a lot for dark theme
    
    * Enable dark theme easily
    
    * add more
    
    * Theme switch

--- themes/simple/tailwind.config.js
+++ themes/simple/tailwind.config.js
@@ -1,6 +1,12 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
+  plugins: [require("tailwindcss-dark-mode")()],
+  variants: {
+    backgroundColor: ['dark', 'dark-hover', 'dark-group-hover'],
+    borderColor: ['dark', 'dark-focus', 'dark-focus-within'],
+    textColor: ['dark', 'dark-hover', 'dark-active']
+  },
   theme: {
     screens: {
       sm: "577px",
@@ -9,6 +15,10 @@ module.exports = {
       xl: "1200px"
     },
     extend: {
+      colors: {
+        dark: "#2d2f34",
+        darker: "#1F2023"
+      },
       fontSize: {
         xs: "1rem",
         sm: "1.125rem",
@@ -20,6 +30,5 @@ module.exports = {
         "4xl": "4rem"
       }
     }
-  },
-  variants: {}
+  }
 };
