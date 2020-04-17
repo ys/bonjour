c812742274abcbcf68fb86c3337e8944bb43176d
Yannick Schutz <hello@yannickschutz.com>
2020-04-17 17:18:40 +0200

    Purge again

--- themes/simple/postcss.config.js
+++ themes/simple/postcss.config.js
@@ -1,6 +1,13 @@
+const themeDir = __dirname + "/";
+
 module.exports = {
   plugins: [
-    require("tailwindcss")("./themes/simple/tailwind.config.js"),
-    require("autoprefixer")
+    require("tailwindcss")(themeDir + "tailwind.config.js"),
+    require("autoprefixer"),
+    require("@fullhuman/postcss-purgecss")({
+      content: [
+        themeDir + "../public/**/*.html"
+      ]
+    })
   ]
 };
