c7c1c27bdf16ef378a2b0fa363eea06035f104f5
Yannick Schutz <github@yannickschutz.com>
2020-04-17 17:43:57 +0200

    Purge again (#172)
    * Purge again
    
    * Purge again try it
    
    * wrap
    
    Co-authored-by: Yannick Schutz <hello@yannickschutz.com>

--- themes/simple/postcss.config.js
+++ themes/simple/postcss.config.js
@@ -1,6 +1,13 @@
+const themeDir = __dirname + "/";
+
 module.exports = {
   plugins: [
-    require("tailwindcss")("./themes/simple/tailwind.config.js"),
+    require("@fullhuman/postcss-purgecss")({
+      content: [
+        themeDir + "../../public/**/*.html"
+      ]
+    }),
+    require("tailwindcss")(themeDir + "tailwind.config.js"),
     require("autoprefixer")
   ]
 };
