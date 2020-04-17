09cc108b366283e9d7c165bf9c64f682bb953552
Yannick Schutz <hello@yannickschutz.com>
2020-04-17 17:36:27 +0200

    wrap

--- themes/simple/postcss.config.js
+++ themes/simple/postcss.config.js
@@ -2,12 +2,12 @@ const themeDir = __dirname + "/";
 
 module.exports = {
   plugins: [
-    require("tailwindcss")(themeDir + "tailwind.config.js"),
-    require("autoprefixer"),
     require("@fullhuman/postcss-purgecss")({
       content: [
         themeDir + "../../public/**/*.html"
       ]
-    })
+    }),
+    require("tailwindcss")(themeDir + "tailwind.config.js"),
+    require("autoprefixer")
   ]
 };
