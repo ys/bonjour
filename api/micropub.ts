ad7f923fa7339c164ff4fd2cc0a7a5d75469cd52
Yannick Schutz <hello@yannickschutz.com>
2020-06-06 21:00:37 +0200

    Add info

--- api/micropub.ts
+++ api/micropub.ts
@@ -1,6 +1,25 @@
 import { NowRequest, NowResponse } from '@vercel/node'
 
 export default (req: NowRequest, res: NowResponse) => {
-  console.log(req)
-  res.json({ name: 'John', email: 'john@example.com' })
+  if (req.method === "GET) {
+    return get(req, res);
+  } else if (req.method === "POST") {
+    return post(req, res);
+  }
+}
+
+function post(req: NowRequest, res: NowResponse) {
+}
+
+function get(req: NowRequest, res: NowResponse) {
+ if (req.query.q === "config") {
+    res.send({
+      "media-endpoint": `https://${req.headers.host}/micropub`
+      // syndicate-to
+    });
+  } else {
+		res.header("Link", '<https://${req.headers.host}/api/micropub>; rel="micropub"')
+		)
+    res.send({})
+  }
 }
