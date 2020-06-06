51517104a9b46deedea176e9fb3561feb5346ff3
Yannick Schutz <hello@yannickschutz.com>
2020-06-06 09:52:45 +0200

    Now

--- api/micropub.ts
+++ api/micropub.ts
@@ -1,5 +1,6 @@
 import { NowRequest, NowResponse } from '@vercel/node'
 
 export default (req: NowRequest, res: NowResponse) => {
+  console.log(req)
   res.json({ name: 'John', email: 'john@example.com' })
 }
