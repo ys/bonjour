42036861cbc25507f6f4ceec666d23476ad697b3
Yannick Schutz <hello@yannickschutz.com>
2021-01-07 22:21:57 +0100

    Clean plugins

--- themes/simple/postcss.config.js
+++ themes/simple/postcss.config.js
@@ -1,23 +1,25 @@
 const themeDir = __dirname + "/";
 
+plugins = []
+if (process.env.HUGO_ENVIRONMENT === 'production') {
+  plugins = [
+    require("@fullhuman/postcss-purgecss")({
+      content: [
+        themeDir + "../../public/**/*.html"
+      ],
+      options: {
+        safelist: [
+          /content-/,
+          /post/,
+          /photoset/,
+          /always/,
+          /max/
+        ]
+      }
+    })]
+}
+plugins.push(require("tailwindcss")(themeDir + "tailwind.config.js"))
+plugins.push(require("autoprefixer"))
 module.exports = {
-  plugins: [
-    require("tailwindcss")(themeDir + "tailwind.config.js"),
-    require("autoprefixer"),
-    ...process.env.HUGO_ENVIRONMENT === 'production'
-      ? [require("@fullhuman/postcss-purgecss")({
-            content: [
-              themeDir + "../../public/**/*.html"
-            ],
-            options: {
-              safelist: [
-                /content-/,
-                /post/,
-                /photoset/,
-                /always/,
-                /max/
-              ]
-            }
-          })] : []
-  ]
+  plugins: plugins
 };
