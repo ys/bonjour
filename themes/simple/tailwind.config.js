df4792b6972a619b46908b169a66fde3c97ec730
Yannick Schutz <yschutz@salesforce.com>
2021-01-07 17:24:23 +0100

    Fixes

--- themes/simple/tailwind.config.js
+++ themes/simple/tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require('tailwindcss/colors');
 
 module.exports = {
   darkMode: "media",
@@ -6,28 +7,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        teal: {
-          100: '#e6fffa',
-          200: '#b2f5ea',
-          300: '#81e6d9',
-          400: '#4fd1c5',
-          500: '#38b2ac',
-          600: '#319795',
-          700: '#2c7a7b',
-          800: '#285e61',
-          900: '#234e52',
-        },
-        orange: {
-          100: '#fffaf0',
-          200: '#feebc8',
-          300: '#fbd38d',
-          400: '#f6ad55',
-          500: '#ed8936',
-          600: '#dd6b20',
-          700: '#c05621',
-          800: '#9c4221',
-          900: '#7b341e',
-        },
+        teal: colors.teal,
+        orange: colors.orange,
+        amber: colors.amber,
+        bluegray: colors.blueGray,
+        rose: colors.rose,
         sunray: {
           100: '#FDF8EE',
           200: '#FBECD5',
