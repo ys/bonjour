4a564c4e78f8a6843231c1b0487add71d435847e
Yannick Schutz <yschutz@salesforce.com>
2021-01-08 17:36:14 +0100

    Help

--- themes/simple/postcss.config.js
+++ themes/simple/postcss.config.js
@@ -1,7 +1,7 @@
 const themeDir = __dirname + "/";
 
 const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: [ '../../hugo_stats.json' ],
+    content: [ themeDir + '../../hugo_stats.json' ],
     defaultExtractor: (content) => {
         let els = JSON.parse(content).htmlElements;
         return els.tags.concat(els.classes, els.ids);
