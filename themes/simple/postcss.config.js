401c43a7530fe39cfc30471be65cce1d09008ff8
Yannick Schutz <hello@yannickschutz.com>
2021-01-30 21:41:50 +0100

    Background

--- themes/simple/postcss.config.js
+++ themes/simple/postcss.config.js
@@ -2,6 +2,7 @@ const themeDir = __dirname + "/";
 
 const purgecss = require('@fullhuman/postcss-purgecss')({
     content: [ themeDir + '../../hugo_stats.json' ],
+    safelist: ["medium-zoom-overlay"],
     defaultExtractor: (content) => {
         let els = JSON.parse(content).htmlElements;
         return els.tags.concat(els.classes, els.ids);
