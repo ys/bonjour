7d64a8eb01e968ff867c7d01c0491b4e4b50ef47
Yannick Schutz <hello@yannickschutz.com>
2021-02-14 20:08:59 +0100

    See for darkmode later

--- themes/simple/tailwind.config.js
+++ themes/simple/tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  darkMode: "media",
+  darkMode: "class",
   plugins: [require('@tailwindcss/typography'),],
   variants: {
     extend: {
