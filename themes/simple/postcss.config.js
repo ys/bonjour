2ecf60e1a80579d29a551ed910269cdc9d6a337a
Yannick Schutz <yschutz@salesforce.com>
2021-02-22 14:20:54 +0100

    LOL

--- themes/simple/postcss.config.js
+++ themes/simple/postcss.config.js
@@ -1,17 +1,20 @@
 const themeDir = __dirname + "/";
 
 const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: [ themeDir + '../../hugo_stats.json' ],
-    defaultExtractor: (content) => {
-        let els = JSON.parse(content).htmlElements;
-        return els.tags.concat(els.classes, els.ids);
-    }
+  content: [themeDir + '../../hugo_stats.json'],
+  safelist: {
+    standard: [/pswp/],
+  },
+  defaultExtractor: (content) => {
+    let els = JSON.parse(content).htmlElements;
+    return els.tags.concat(els.classes, els.ids);
+  }
 });
 
 module.exports = {
   plugins: [
     require("tailwindcss")(themeDir + "tailwind.config.js"),
     require("autoprefixer"),
-    ...(process.env.HUGO_ENVIRONMENT === 'production' ? [ purgecss ] : [])
+    ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
   ]
 };
