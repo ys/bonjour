e69d09fea20d27e048a5a0903a1fdbfc23140f61
Yannick Schutz <yschutz@salesforce.com>
2021-02-22 14:12:54 +0100

    fix swipe with style

--- themes/simple/assets/js/main.js
+++ themes/simple/assets/js/main.js
@@ -2,7 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
   new LazyLoad({
     elements_selector: ".lazyload"
   });
-  photoswipeSimplify.init();
+  photoswipeSimplify.init({
+    history: false,
+    bgOpacity: 1,
+    zoomEl: false,
+    shareEl: false,
+    counterEl: false,
+    shareButtons: [],
+    fullscreenEl: false,
+    closeEl: false,
+    captionEl: false,
+    barsSize: { top: 44, bottom: 44, left: 44, right: 44 }
+  });
 })
 
 
