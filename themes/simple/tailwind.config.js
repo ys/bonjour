c9fa15f817f9a3d82815184e5b18a806fd4a4eae
Yannick Schutz <github@yannickschutz.com>
2021-12-04 17:26:48 +0000

    Kill the darkness

--- themes/simple/tailwind.config.js
+++ themes/simple/tailwind.config.js
@@ -2,7 +2,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  darkMode: 'class',
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/line-clamp'),
