7b883eaa4c2a023f5fbf185faf64073189392528
Yannick Schutz <hello@yannickschutz.com>
2022-10-13 22:49:03 +0200

    TYPE NO OF

--- themes/simple/assets/js/photoswipe-simplify.js
+++ themes/simple/assets/js/photoswipe-simplify.js
@@ -1,4 +1,3 @@
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 /*!
  * photoswipe-simplify v0.0.3: PhotoSwipe.js simplify by the VanillaJS.
