adda6eb56031817c6943493ad9ed2d7d0d01f54d
Yannick Schutz <hello@yannickschutz.com>
2022-12-21 22:52:11 +0100

    Fix newsletter

--- flat/buttondown.ts
+++ flat/buttondown.ts
@@ -30,7 +30,7 @@ type Letter = {
 const filename = Deno.args[0]
 const data = await readJSON(filename)
 data.results.forEach((letter: Letter) => {
-	let letterName = `./content/bonjour/${letter.secondary_id}.md`
+	let letterName = `./content/bonjour/${letter.secondary_id}/index.md`
 	exists(letterName).then((exist) => {
 		if (!exist) {
 			Deno.writeTextFileSync(letterName,
