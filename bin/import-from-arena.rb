a97cf26f65bc78434cf761a4b3117a9778130414
Yannick Schutz <hello@yannickschutz.com>
2023-02-01 20:35:33 +0100

    Arenam

---
+++ bin/import-from-arena.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+require "rubygems"
+require "bundler/setup"
+require "json"
+require "yaml"
+Bundler.require(:default)
+Dotenv.load
+
+Arena.configure do |config|
+  config.access_token = ENV["ARENA_TOKEN"]
+end
+
+class Link
+  attr_accessor :source, :title, :description, :image
+end
+
+all = []
+i = 0 
+loop do 
+contents = Arena.get("/channels/1977086/contents?page=#{i}")["contents"]
+all = all + contents
+i += 1
+puts contents.size
+break unless contents.size > 0
+end
+
+all = all.reverse
+
+all.reject! { |c| c.dig("source", "url").nil? }
+
+File.open("data/liked.yml", "w") { |file| file.write(YAML.dump(all)) }
+
