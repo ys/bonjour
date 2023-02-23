9d0793bf1a64f7b3b92f29eae4964b0a1add9701
Yannick Schutz <hello@yannickschutz.com>
2023-02-23 21:50:01 +0100

    Discretion

--- themes/simple/tailwind.config.js
+++ themes/simple/tailwind.config.js
@@ -77,66 +77,11 @@ module.exports = {
         darker: "#1F2023"
       },
       typography: (theme) => ({
-        light: {
-          css: [
-            {
-              color: theme('colors.gray.100'),
-              '[class~="lead"]': {
-                color: theme('colors.gray.300'),
-              },
-              strong: {
-                color: theme('colors.white'),
-              },
-              'ol > li::before': {
-                color: theme('colors.gray.400'),
-              },
-              'ul > li::before': {
-                backgroundColor: theme('colors.gray.600'),
-              },
-              hr: {
-                borderColor: theme('colors.gray.200'),
-              },
-              blockquote: {
-                color: theme('colors.gray.200'),
-                borderLeftColor: theme('colors.gray.600'),
-              },
-              h1: {
-                color: theme('colors.white'),
-              },
-              h2: {
-                color: theme('colors.white'),
-              },
-              h3: {
-                color: theme('colors.white'),
-              },
-              h4: {
-                color: theme('colors.white'),
-              },
-              'figure figcaption': {
-                color: theme('colors.gray.400'),
-              },
-              code: {
-                color: theme('colors.white'),
-              },
-              'a code': {
-                color: theme('colors.white'),
-              },
-              pre: {
-                color: theme('colors.gray.200'),
-                backgroundColor: theme('colors.gray.800'),
-              },
-              thead: {
-                color: theme('colors.white'),
-                borderBottomColor: theme('colors.gray.400'),
-              },
-              'tbody tr': {
-                borderBottomColor: theme('colors.gray.600'),
-              },
-            },
-          ],
-        },
         DEFAULT: {
           css: {
+            a: {
+              color: 'var(--tw-prose-body)',
+            },
             img: {
               marginTop: 0,
               marginBottom: 0
