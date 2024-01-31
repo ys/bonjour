12ebc1409f960a52c0c7922ab679eb5d3a3bee2e
Yannick Schutz <yschutz@salesforce.com>
2024-01-31 15:28:00 +0100

    Many fixes

--- flat/italictype.ts
+++ flat/italictype.ts
@@ -72,8 +72,6 @@ const byYear = (b: Book) => {
 	return new Date(b.read).getFullYear()
 }
 const booksbyyear = _.groupBy(books, byYear) as BooksByYear
-await Deno.writeTextFile("./data/books/2021.yaml", yaml.dump({ year: 2021, books: booksbyyear[2021] }));
-await Deno.writeTextFile("./data/books/2022.yaml", yaml.dump({ year: 2022, books: booksbyyear[2022] }));
-await Deno.writeTextFile("./data/books/2023.yaml", yaml.dump({ year: 2023, books: booksbyyear[2023] }));
+await Deno.writeTextFile("./data/books/2024.yaml", yaml.dump({ year: 2024, books: booksbyyear[2024] }));
 
 console.log("New books written down")
