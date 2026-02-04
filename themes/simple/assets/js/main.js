dd5f43b4523705206e5c5e66de437e461b0a74ff
Yannick Schutz <hello@yannickschutz.com>
2026-02-04 20:57:31 +0100

    Add polaroid-style frame to image lightbox

--- themes/simple/assets/js/main.js
+++ themes/simple/assets/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   new LazyLoad({
-    elements_selector: "[loading=lazy]"
+    elements_selector: "[loading=lazy]",
   });
   photoswipeSimplify.init({
     history: false,
@@ -13,72 +13,72 @@ document.addEventListener("DOMContentLoaded", function () {
     captionEl: false,
     arrowEl: false,
     maxSpreadZoom: 1,
-    getDoubleTapZoom: function(isMouseClick, item) {
+    getDoubleTapZoom: function (isMouseClick, item) {
       return 1;
     },
-    barsSize: { top: 44, bottom: 44, left: 44, right: 44 }
+    barsSize: { top: 60, bottom: 120 },
   });
-  params = new URLSearchParams(window.location.search)
+  params = new URLSearchParams(window.location.search);
   if (params.has("zoom")) {
-    photoswipeSimplify.open(0,0);
- }
+    photoswipeSimplify.open(0, 0);
+  }
 
   if (params.has("session_id")) {
     fetch("/api/success?session_id=" + params.get("session_id"), {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-    }).then( res => res.json() )
-    .then( res => {
-      document.querySelector(".thanks").innerText = "🎉 Merci pour cet achat, " + res.shipping.name
-      document.querySelector("#thanksdiv").classList.toggle("hidden");
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
     })
-
+      .then((res) => res.json())
+      .then((res) => {
+        document.querySelector(".thanks").innerText =
+          "🎉 Merci pour cet achat, " + res.shipping.name;
+        document.querySelector("#thanksdiv").classList.toggle("hidden");
+      });
   }
 
-  form = document.querySelector('#stripe-form');
+  form = document.querySelector("#stripe-form");
   if (form) {
-    form.addEventListener('submit', handleFormSubmission);
+    form.addEventListener("submit", handleFormSubmission);
   }
-})
+});
 
 function handleFormSubmission(event) {
   event.preventDefault();
 
-  var htmlForm = document.getElementById('stripe-form');
+  var htmlForm = document.getElementById("stripe-form");
   if (htmlForm.checkValidity() == false) {
-    var list = htmlForm.querySelectorAll(':invalid');
+    var list = htmlForm.querySelectorAll(":invalid");
     for (var item of list) {
       item.focus();
     }
     return null;
   }
 
-  var submit = document.getElementById('checkout-button')
-  submit.querySelector('svg').classList.toggle('hidden')
-  submit.querySelector('span').classList.toggle('hidden')
+  var submit = document.getElementById("checkout-button");
+  submit.querySelector("svg").classList.toggle("hidden");
+  submit.querySelector("span").classList.toggle("hidden");
 
   form = new FormData(event.target);
 
   data = {
-    price_id: form.get('price_id'),
-    shipping_rate: form.get('shipping_rate'),
-    slug: window.location.pathname
+    price_id: form.get("price_id"),
+    shipping_rate: form.get("shipping_rate"),
+    slug: window.location.pathname,
   };
 
-  fetch('/api/checkout', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+  fetch("/api/checkout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
-  }).then( res => res.json() )
-    .then( response => {
+  })
+    .then((res) => res.json())
+    .then((response) => {
       stripe = Stripe(response.publishableKey);
-      stripe.redirectToCheckout({sessionId: response.sessionId});
-    }).then( res => {
+      stripe.redirectToCheckout({ sessionId: response.sessionId });
+    })
+    .then((res) => {
       if (res.error()) {
         console.error(res.error());
       }
     });
-
 }
-
-
